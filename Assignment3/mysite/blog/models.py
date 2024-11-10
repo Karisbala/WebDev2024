fde1f216a1708cc94da00825b984e09e7e575357
Abzal <issayev.abzal@gmail.com>
2024-11-10 15:55:17 +0500

    Created Category and Comment models and registered them in admin panel

--- Assignment3/mysite/blog/models.py
+++ Assignment3/mysite/blog/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.CharField(max_length=100)
     published_date = models.DateTimeField(default=timezone.now)
+    categories = models.ManyToManyField(Category, related_name='posts')
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post.title}"
