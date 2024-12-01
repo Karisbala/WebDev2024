d8739876a0dead5ac316eff8ebca4bca6aabaf46
Abzal <issayev.abzal@gmail.com>
2024-12-01 16:10:15 +0500

    added post and comment creation in admin panel

--- Assignment4/blog_project/blog_app/admin.py
+++ Assignment4/blog_project/blog_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'timestamp')
+    search_fields = ('title', 'content')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'timestamp')
+    search_fields = ('content',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
